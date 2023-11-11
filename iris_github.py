324fdbc447dec87da735b045bea647839ef31f62
pengshihyu <117267076+pengshihyu@users.noreply.github.com>
2023-11-11 15:13:55 +0800

    iris_github.py

--- iris_github.py
+++ iris_github.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import load_iris
+source /home/adminuser/venv/bin/activate
+
 
 # from sklearn.datasets import load_iris
 # iris = load_iris()
