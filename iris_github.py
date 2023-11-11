513f8631e79b1e542c52f21cdc9dab4f7ed247ad
pengshihyu <117267076+pengshihyu@users.noreply.github.com>
2023-11-11 15:48:38 +0800

    Update iris_github.py

--- iris_github.py
+++ iris_github.py
@@ -1,35 +1,42 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import load_iris
 
-iris = load_iris()
-df_data = pd.DataFrame(data=np.c_[iris['data'], iris['target']],
-                        columns=['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm', 'Species'])
+# from sklearn.datasets import load_iris
+# iris = load_iris()
+# # 輸入特徵
+# X = iris.data
+# # 輸出特徵
+# y = iris.target
 
-# 直方图 histograms
-df_data.hist(alpha=0.6, layout=(3, 3), figsize=(12, 8), bins=10)
+iris = load_iris()
+df_data = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
+                     columns= ['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm','Species'])
+# df_data
+
+#直方圖 histograms
+df_data.hist(alpha=0.6,layout=(3,3), figsize=(12, 8), bins=10) 
 plt.tight_layout()
 plt.show()
 
-fig, axes = plt.subplots(nrows=1, ncols=4)
+fig, axes = plt.subplots(nrows=1,ncols=4)
 fig.set_size_inches(15, 4)
-sns.histplot(df_data["SepalLengthCm"][:], ax=axes[0], kde=True)
-sns.histplot(df_data["SepalWidthCm"][:], ax=axes[1], kde=True)
-sns.histplot(df_data["PetalLengthCm"][:], ax=axes[2], kde=True)
-sns.histplot(df_data["PetalWidthCm"][:], ax=axes[3], kde=True)
+sns.histplot(df_data["SepalLengthCm"][:],ax=axes[0], kde=True)
+sns.histplot(df_data["SepalWidthCm"][:],ax=axes[1], kde=True)
+sns.histplot(df_data["PetalLengthCm"][:],ax=axes[2], kde=True)
+sns.histplot(df_data["PetalWidthCm"][:],ax=axes[3], kde=True)
 
 from pandas.plotting import scatter_matrix
-scatter_matrix(df_data, figsize=(10, 10), color='b', diagonal='kde')
+scatter_matrix( df_data,figsize=(10, 10),color='b',diagonal='kde')
 
 sns.pairplot(df_data, hue="Species", height=2, diag_kind="kde")
 
-# correlation 计算
-corr = df_data[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']].corr()
-plt.figure(figsize=(8, 8))
+# correlation 計算
+corr = df_data[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm','Species']].corr()
+plt.figure(figsize=(8,8))
 sns.heatmap(corr, square=True, annot=True, cmap="RdBu_r")
 
-sns.lmplot("SepalLengthCm", "SepalWidthCm", hue='Species', data=df_data, fit_reg=False)
+sns.lmplot("SepalLengthCm", "SepalWidthCm", hue='Species', data=df_data, fit_reg=False, legend=False)
 plt.legend(title='Species', loc='upper right', labels=['Iris-Setosa', 'Iris-Versicolour', 'Iris-Virginica'])
-plt.show()
