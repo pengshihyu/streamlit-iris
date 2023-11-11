548364e4253bc50efbfe4a14ac0159c6175be53e
pengshihyu <117267076+pengshihyu@users.noreply.github.com>
2023-11-11 15:13:03 +0800

    iris_github.py

--- iris_github.py
+++ iris_github.py
@@ -1,8 +1,3 @@
-pip install --upgrade matplotlib
-pip install seaborn
-pip install scikit-learn
-pip install --upgrade streamlit
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
